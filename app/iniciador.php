e01a85fa34f983166c895ec50db71138e66d8fa7
Alex <alexalonsoportas@gmail.com>
2020-11-17 20:14:37 +0100

    me falla la url

--- app/iniciador.php
+++ app/iniciador.php
@@ -1,1 +1,6 @@
-HOLA MUNDO!!
+<?php 
+
+	//Cargar librerias
+	require_once "librerias/Base.php";
+	require_once "librerias/Controlador.php";
+	require_once "librerias/Core.php";
--- public/index.php
+++ public/index.php
@@ -1,3 +1,4 @@
 <?php
 	require_once "../app/iniciador.php";
+	$iniciar = new Core;
 ?>
